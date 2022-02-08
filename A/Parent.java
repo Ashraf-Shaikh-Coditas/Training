0b3b8aa4781cf0d9f9e5e5abe74a3061086fdb7e
Ashraf Shaikh <ashraf.shaikh@coditas.com>
2022-02-08 11:43:09 +0530

    First commit

---
+++ A/Parent.java
@@ -0,0 +1,11 @@
+package Week3.Session13.A;
+
+ public class Parent {
+    public void display() {
+        System.out.println("This is our Parent class method");
+    }
+
+    protected void show() {
+        System.out.println("This is show() method from Parent class");
+    }
+}
---
+++ B/Child.java
@@ -0,0 +1,17 @@
+package Week3.Session13.B;
+
+import Week3.Session13.A.Parent;
+
+public class Child extends Parent {
+    public static void main(String[] args) {
+        Child obj_1 = new Child();
+        obj_1.display();
+        obj_1.show();
+
+    }
+}
+/*
+
+This is our Parent class method
+
+* */
